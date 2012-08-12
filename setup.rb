e698ed93a156fc34bb1bee760679c38b096fa4f8
fumin <awawfumin@gmail.com>
2012-08-13 01:14:55 +0800

    fix database connection pool size

--- setup.rb
+++ setup.rb
@@ -1,8 +1,21 @@
 require 'active_record'
 
 def setup_db_conn
-  ActiveRecord::Base.establish_connection(
-    ENV['DATABASE_URL'] || YAML::load(File.open('config/database.yml'))['development'])
+  if ENV['DATABASE_URL']
+    db_env = URI.parse(ENV['DATABASE_URL'])
+    db = {}
+    db['adapter'] = (db.scheme == 'postgres' ? 'postgresql' : db.scheme)
+    db['host'] = db.host
+    db['port'] = db.port
+    db['username'] = db.user
+    db['password'] = db.password
+    db['database'] = db.path[1..-1]
+    db['pool'] = 1000
+    db['encoding'] = 'utf8'
+  else
+    db = YAML::load(File.open('config/database.yml'))['development']
+  end
+  ActiveRecord::Base.establish_connection(db)
 end
 
 def init_app
